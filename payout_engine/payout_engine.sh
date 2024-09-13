e7f9333637014daae455b7170d5afc9e923230cd
elementalennui-dev <elementalennui@gmail.com>
2024-09-13 06:07:12 +0000

    venv

--- payout_engine/payout_engine.sh
+++ payout_engine/payout_engine.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+# Activate the virtual environment
+source ~/myenv/bin/activate
+
+# Run the Python script
 sudo python3 -W ignore ~/banano-sports-pools/payout_engine/main.py
+
+deactivate
